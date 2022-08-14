7cfadb9d6712ea475a17957a113aa4bd20e7be9c
Bcypher01 <45035734+Bcypher01@users.noreply.github.com>
2022-08-14 20:30:37 +0100

    fix validation bug

--- Everlytics/form.js
+++ Everlytics/form.js
@@ -1,7 +1,18 @@
+const pipelineEl = document.querySelector("#pipeline");
+const projectEl = document.querySelector("#project");
+const bucketEl = document.querySelector("#bucket");
+const filesEl = document.querySelector("#files");
+const credentialsEl = document.querySelector("#credentials");
+const runEl = document.querySelector("#run");
+
+const form = document.querySelector("#form");
+
 function validate(input, inputEl) {
   valid = false;
   if (!isRequired(input)) {
     showError(inputEl, "This field Cannot be empty");
+  } else if (!minLength(input.length)) {
+    showError(inputEl, "This field cannot be less than 5 characters");
   } else if (isValidCharacter(input)) {
     showError(inputEl, "Cannot have special characters");
   } else {
@@ -12,20 +23,11 @@ function validate(input, inputEl) {
   return valid;
 }
 
-const pipelineEl = document.querySelector("#pipeline");
-const projectEl = document.querySelector("#project");
-const bucketEl = document.querySelector("#bucket");
-const filesEl = document.querySelector("#files");
-const credentialsEl = document.querySelector("#credentials");
-const runEl = document.querySelector("#run");
-
-const form = document.querySelector("#form");
-
 const isRequired = (value) => (value === "" ? false : true);
-const minLength = (length, min) => (length < min ? false : true);
+const minLength = (length) => (length < 5 ? false : true);
 const isValidCharacter = (value) => {
-  const re = new RegExp("/^[a-zA-Z]*$/");
-  return re.test(value);
+  const re = new RegExp("^(?![_|-])");
+  return !re.test(value);
 };
 
 const showError = (input, message) => {
@@ -109,5 +111,6 @@ form.addEventListener("submit", function (e) {
 
   // submit to the server if the form is valid
   if (isFormValid) {
+    alert("Welcome!!!");
   }
 });
